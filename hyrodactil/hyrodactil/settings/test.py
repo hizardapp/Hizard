7983ecc32735e2fa8157c8c163ad334758a1cfab
Keats <balthek@gmail.com>
2013-01-19 12:51:37 +0000

    initial commit

---
+++ hyrodactil/hyrodactil/settings/test.py
@@ -0,0 +1,19 @@
+from base import *
+
+########## TEST SETTINGS
+TEST_RUNNER = 'discover_runner.DiscoverRunner'
+TEST_DISCOVER_TOP_LEVEL = DJANGO_ROOT
+TEST_DISCOVER_ROOT = DJANGO_ROOT
+TEST_DISCOVER_PATTERN = "*"
+
+########## IN-MEMORY TEST DATABASE
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+        },
+    }
