60ad7c1f96b9e29462dcc48035b9d54b77e1b883
Keats <balthek@gmail.com>
2013-01-20 18:58:04 +0000

    moving tests to its own directory

---
+++ hyrodactil/tests/factories/jobs.py
@@ -0,0 +1,33 @@
+import factory
+
+from .companies import CompanyFactory, QuestionFactory
+from jobs.models import Application, ApplicationAnswer, Opening
+
+
+class OpeningFactory(factory.Factory):
+    FACTORY_FOR = Opening
+
+    title = 'Salesman'
+    description = 'Sales stuff'
+    is_private = ''
+    loc_country = 'FR'
+    loc_city = 'Cannes'
+    loc_postcode = '93100'
+    company = factory.SubFactory(CompanyFactory)
+    #questions = factory.SubFactory(QuestionFactory)
+
+
+class ApplicationFactory(factory.Factory):
+    FACTORY_FOR = Application
+
+    first_name = 'Bilbon'
+    last_name = 'Sacquet'
+    opening = factory.SubFactory(OpeningFactory)
+
+
+class ApplicationAnswerFactory(factory.Factory):
+    FACTORY_FOR = ApplicationAnswer
+
+    answer = "Some clever answer"
+    application = factory.SubFactory(ApplicationFactory)
+    question = factory.SubFactory(QuestionFactory)
