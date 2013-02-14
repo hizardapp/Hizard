eee15f1c9ee4247edf4c1d08be04e8b471ac1c7c
Keats <balthek@gmail.com>
2013-02-14 23:03:25 +0000

    django: no need to type --settings=... when running tests anymore

--- hyrodactil/manage.py
+++ hyrodactil/manage.py
@@ -3,7 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hyrodactil.settings.dev")
+
+    if sys.argv[1] == 'test':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hyrodactil.settings.test")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hyrodactil.settings.dev")
 
     from django.core.management import execute_from_command_line
 
