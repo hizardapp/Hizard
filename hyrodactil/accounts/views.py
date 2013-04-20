b0089f336f8ee615268abe0a1a9fd1334271068a
Gautier Hayoun <ghayoun@gmail.com>
2013-04-20 13:29:04 +0100

    accounts: Remove unused code

--- hyrodactil/accounts/views.py
+++ hyrodactil/accounts/views.py
@@ -20,7 +20,6 @@ from .models import CustomUser
 
 
 class RegistrationView(CreateView):
-    model = CustomUser
     form_class = UserCreationForm
     success_url = reverse_lazy('accounts:register_confirmation')
     template_name = 'accounts/registration_form.html'
