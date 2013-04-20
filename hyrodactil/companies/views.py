64894824e8f84487e7ffd6ca43b5435445a5d769
Gautier Hayoun <ghayoun@gmail.com>
2013-04-20 13:29:29 +0100

    companies: Redirect to Home after company is created

--- hyrodactil/companies/views.py
+++ hyrodactil/companies/views.py
@@ -15,7 +15,7 @@ class CompanyCreateView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         return utils.build_subdomain_url(
             self.request,
-            reverse('openings:list_openings')
+            reverse('public:home')
         )
 
     def form_valid(self, form):
--- hyrodactil/tests/companies/views.py
+++ hyrodactil/tests/companies/views.py
@@ -39,7 +39,7 @@ class CompaniesViewsTests(WebTest):
         form['subdomain'] = 'acmememe'
         response = form.submit()
         expected = "http://acmememe.%s%s" % (
-            settings.SITE_URL, reverse("openings:list_openings")
+            settings.SITE_URL, reverse("public:home")
         )
         self.assertEqual(response["Location"], expected)
         self.assertEqual(response.status_code, 302)
