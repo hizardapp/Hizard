ea5c808db2d1deb57f86fd12b7b2c1547326b1af
Keats <balthek@gmail.com>
2013-04-20 11:28:27 +0100

    prod: add sentry

--- hyrodactil/hyrodactil/settings/production.py
+++ hyrodactil/hyrodactil/settings/production.py
@@ -15,3 +15,11 @@ DATABASES = {
 
 STATIC_ROOT = '/home/app/static/'
 MEDIA_URL = 'http://hizard.com/media/'
+
+RAVEN_CONFIG = {
+    'dsn': 'https://440d63c598e04fde809ebb8f64495b7d:e5d86579a5ad4d61855009b0da779a9e@app.getsentry.com/7604',
+}
+
+INSTALLED_APPS = INSTALLED_APPS + (
+    'raven.contrib.django.raven_compat',
+)
