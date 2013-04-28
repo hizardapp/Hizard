b84113d1717782041346ed331e68c0f3e863dcf7
Gautier Hayoun <ghayoun@gmail.com>
2013-04-28 16:06:15 +0100

    core: Do not cache template in development

--- hyrodactil/hyrodactil/settings/dev.py
+++ hyrodactil/hyrodactil/settings/dev.py
@@ -12,6 +12,11 @@ DEBUG = True
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
 TEMPLATE_DEBUG = DEBUG
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
 ########## END DEBUG CONFIGURATION
 
 
