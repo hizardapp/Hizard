93a7d3b6756abc44832c13722a283145d27735e4
Gautier Hayoun <ghayoun@gmail.com>
2013-04-28 15:22:11 +0100

    core: Remove admin from urls

--- hyrodactil/hyrodactil/urls.py
+++ hyrodactil/hyrodactil/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 
-from django.contrib import admin
-admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
     url(
         r'',
         include(
