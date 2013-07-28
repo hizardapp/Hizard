7edecb8b3f4a86df10ad860f204607a4b0453261
Keats <balthek@gmail.com>
2013-07-28 15:43:58 +0100

    new design: did companies form

--- hyrodactil/companies/forms.py
+++ hyrodactil/companies/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Company
 
@@ -7,3 +8,10 @@ class CompanyForm(forms.ModelForm):
     class Meta:
         model = Company
         fields = ('name', 'subdomain', 'website', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super(CompanyForm, self).__init__(*args, **kwargs)
+        self.fields['name'].label = _('Company name')
+        self.fields['subdomain'].label = _('Url of your career site (***.hizard.com)')
+        self.fields['website'].label = _('Company website (will appear on career site)')
+        self.fields['description'].label = _('Company description (will appear on career site)')
