97200da4e6ab8ea7cd3b1dda093cdbdc7faaa3e1
Keats <balthek@gmail.com>
2013-07-28 19:25:56 +0100

    general: updated foundation to 4.3

--- hyrodactil/static/js/foundation/foundation.js
+++ hyrodactil/static/js/foundation/foundation.js
@@ -4,7 +4,7 @@
  * Copyright 2013, ZURB
  * Free to use under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- */
+*/
 
 /*jslint unparam: true, browser: true, indent: 2 */
 
@@ -29,19 +29,61 @@ if (typeof jQuery === "undefined" &&
 (function ($, window, document, undefined) {
   'use strict';
 
+  /*
+    matchMedia() polyfill - Test a CSS media 
+    type/query in JS. Authors & copyright (c) 2012: 
+    Scott Jehl, Paul Irish, Nicholas Zakas. 
+    Dual MIT/BSD license
+
+    https://github.com/paulirish/matchMedia.js
+  */
+
+  window.matchMedia = window.matchMedia || (function( doc, undefined ) {
+
+    "use strict";
+
+    var bool,
+        docElem = doc.documentElement,
+        refNode = docElem.firstElementChild || docElem.firstChild,
+        // fakeBody required for <FF4 when executed in <head>
+        fakeBody = doc.createElement( "body" ),
+        div = doc.createElement( "div" );
+
+    div.id = "mq-test-1";
+    div.style.cssText = "position:absolute;top:-100em";
+    fakeBody.style.background = "none";
+    fakeBody.appendChild(div);
+
+    return function(q){
+
+      div.innerHTML = "&shy;<style media=\"" + q + "\"> #mq-test-1 { width: 42px; }</style>";
+
+      docElem.insertBefore( fakeBody, refNode );
+      bool = div.offsetWidth === 42;
+      docElem.removeChild( fakeBody );
+
+      return {
+        matches: bool,
+        media: q
+      };
+
+    };
+
+  }( document ));
+
   // add dusty browser stuff
   if (!Array.prototype.filter) {
     Array.prototype.filter = function(fun /*, thisp */) {
       "use strict";
-
+   
       if (this == null) {
         throw new TypeError();
       }
 
       var t = Object(this),
           len = t.length >>> 0;
-      if (typeof fun != "function") {
-        return;
+      if (typeof fun !== "function") {
+          return;
       }
 
       var res = [],
@@ -65,20 +107,20 @@ if (typeof jQuery === "undefined" &&
         // closest thing possible to the ECMAScript 5 internal IsCallable function
         throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
       }
-
-      var aArgs = Array.prototype.slice.call(arguments, 1),
-          fToBind = this,
+   
+      var aArgs = Array.prototype.slice.call(arguments, 1), 
+          fToBind = this, 
           fNOP = function () {},
           fBound = function () {
             return fToBind.apply(this instanceof fNOP && oThis
-                ? this
-                : oThis,
-                aArgs.concat(Array.prototype.slice.call(arguments)));
+               ? this
+               : oThis,
+             aArgs.concat(Array.prototype.slice.call(arguments)));
           };
-
+   
       fNOP.prototype = this.prototype;
       fBound.prototype = new fNOP();
-
+   
       return fBound;
     };
   }
@@ -104,7 +146,7 @@ if (typeof jQuery === "undefined" &&
         }
       }
       if (n >= len) {
-        return -1;
+          return -1;
       }
       var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
       for (; k < len; k++) {
@@ -124,9 +166,8 @@ if (typeof jQuery === "undefined" &&
   window.Foundation = {
     name : 'Foundation',
 
-    version : '4.1.5',
+    version : '4.3.1',
 
-    // global Foundation cache object
     cache : {},
 
     init : function (scope, libraries, method, options, response, /* internal */ nc) {
@@ -145,7 +186,7 @@ if (typeof jQuery === "undefined" &&
       // set foundation global scope
       this.scope = scope || this.scope;
 
-      if (libraries && typeof libraries === 'string') {
+      if (libraries && typeof libraries === 'string' && !/reflow/i.test(libraries)) {
         if (/off/i.test(libraries)) return this.off();
 
         library_arr = libraries.split(' ');
@@ -156,6 +197,8 @@ if (typeof jQuery === "undefined" &&
           }
         }
       } else {
+        if (/reflow/i.test(libraries)) args[1] = 'reflow';
+
         for (var lib in this.libs) {
           responses.push(this.init_lib(lib, args));
         }
@@ -188,6 +231,8 @@ if (typeof jQuery === "undefined" &&
         if (this.libs.hasOwnProperty(lib)) {
           this.patch(this.libs[lib]);
           return this.libs[lib].init.apply(this.libs[lib], args);
+        } else {
+          return function () {};
         }
       }.bind(this), lib);
     },
@@ -221,7 +266,7 @@ if (typeof jQuery === "undefined" &&
     },
 
     random_str : function (length) {
-      var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+      var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
 
       if (!length) {
         length = Math.floor(Math.random() * chars.length);
@@ -356,7 +401,7 @@ if (typeof jQuery === "undefined" &&
         return el.outerHeight();
       };
 
-      lib.outerWidth = function (el) {
+      lib.outerWidth = function (el, bool) {
         if (typeof Zepto === 'function') {
           return el.width();
         }
@@ -380,13 +425,7 @@ if (typeof jQuery === "undefined" &&
       return true;
     },
 
-    zj : function () {
-      try {
-        return Zepto;
-      } catch (e) {
-        return jQuery;
-      }
-    }()
+    zj : $
   };
 
   $.fn.foundation = function () {
